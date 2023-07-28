b197b5024572ba0858b99e7f565210dc16a29c62
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-28 08:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+ document.addEventListener("DOMContentLoaded", function() {
+      const gridContainer = document.getElementById("grid-container");
+      
+      // Create 6 block elements and add them to the grid container
+      for (let i = 0; i < 6; i++) {
+        const blockElement = document.createElement("div");
+        blockElement.classList.add("block");
+        gridContainer.appendChild(blockElement);
+      }
+    });
